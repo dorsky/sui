83d20f37b28aef89ea7305c981a2bc128bd3ced1
Mark Logan <mark@mystenlabs.com>
2023-11-13 13:35:36 -0800

    wip

--- crates/sui-types/src/transaction.rs
+++ crates/sui-types/src/transaction.rs
@@ -2937,6 +2937,12 @@ impl From<Object> for ReceivingObjectReadResultKind {
     }
 }
 
+impl From<Arc<Object>> for ReceivingObjectReadResultKind {
+    fn from(object: Arc<Object>) -> Self {
+        Self::Object(object)
+    }
+}
+
 pub struct ReceivingObjects {
     pub objects: Vec<ReceivingObjectReadResult>,
 }
